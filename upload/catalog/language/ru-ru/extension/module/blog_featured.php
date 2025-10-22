e9908924822be1b435d35b59ab0f227ad012a060
19th <19th19th@gmail.com>
2025-10-22 17:10:27 +0600

    Исправление перевода в блоге

--- upload/catalog/language/ru-ru/extension/module/blog_featured.php
+++ upload/catalog/language/ru-ru/extension/module/blog_featured.php
@@ -1,11 +1,8 @@
 <?php
-// *	@source		See SOURCE.txt for source and other copyright.
-// *	@license	GNU General Public License version 3; see LICENSE.txt
-
-// Heading 
+// Heading
 $_['heading_title'] = 'Рекомендуем';
 $_['text_views'] 	= 'Просмотров:';
-$_['button_more']   = 'подробнее';
+$_['button_more']   = 'Подробнее';
 
 // Text
 $_['text_reviews']  = 'На основании %s отзывов.'; 
